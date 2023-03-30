c35374210fcb242b46393dcbd00cf829b17afc20
Hyojin Lim <111869216+dinmoy@users.noreply.github.com>
2023-03-30 17:04:57 +0900

    자연수가 들어있는 배열의 평균을 구하고 평균 이하인 숫자는 몇개 인지 구하기

--- Solution.java
+++ Solution.java
@@ -1,22 +1,26 @@
-package Ex09;
+package Ex10;
 
 public class Solution {
-	public String solution(String characters) {
-		String result="";
-		result+=characters.charAt(0);
-		for(int i=0;i<characters.length()-1;i++) {
-			if(characters.charAt(i)!=characters.charAt(i+1))
-				result+=characters.charAt(i+1);
-			
-		}
-		return result;
+	public int solution(int [] data) {
+		double total=0;
+		int len=data.length;
+		for(int i=0;i<len;i++) 
+			total+=data[i];
+		int cnt=0;
+		double average=total/len;
+		for(int i=0;i<len;i++)
+			if(data[i]<average) cnt++;
+		return cnt;
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Solution sol=new Solution();
-		String characters="senteeeencccceee";
-		String ret=sol.solution(characters);
-		System.out.println("Solution: return value of the method is "+ret+".");
+		int[] data1= {1,2,3,4,5,6,7,8,9,10};
+		int ret1=sol.solution(data1);
+		System.out.println("SolutionL return value of the method is "+ret1+".");
+		int[] data2= {1,1,1,1,1,1,1,1,1,10};
+		int ret2=sol.solution(data2);
+		System.out.println("Solution: return value of the method is "+ret2+".");
 	}
 
 }
